16a0eb1404199fe2cc86d06958fa9db8a86c1856
Shivansh235 <shivanshkasaudhan96@gmail.com>
2026-09-06 16:19:30 +0530

    Add commit

--- src/db/server.js
+++ src/db/server.js
@@ -4,8 +4,10 @@ import { DB_NAME } from "../constants.js";
 // Db is in another continent for that we use asynct to control time and sometime err is also came from database so we use try catch also
 const connectDB = async () => {
   try {
-   const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
-    console.log("\n Database connected successfully: " `${connectionInstance.connection.host}`);
+   const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(
+  `\n Database connected successfully: ${connectionInstance.connection.host}`
+);
     } catch (err) {
     console.log("MONGO DB CONNECTION ERROR ", err);
     process.exit(1);
